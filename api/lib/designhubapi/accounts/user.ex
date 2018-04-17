765b47ddcb2d9b53e554e6a654f6e1bbb74151a1
Rakesh Chatrath <rakesh@rakeshchatrath.me>
2018-04-16 20:20:40 -0500

    Add unique constraint fields

--- api/lib/designhubapi/accounts/user.ex
+++ api/lib/designhubapi/accounts/user.ex
@@ -17,5 +17,7 @@ defmodule Designhubapi.Accounts.User do
     user
     |> cast(attrs, [:username, :email, :password_hash, :name])
     |> validate_required([:username, :email, :password_hash, :name])
+    |> unique_constraint(:username)
+    |> unique_constraint(:email)
   end
 end
